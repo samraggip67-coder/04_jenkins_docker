b5fdeb45500cb54d64889103f2652f8f90b4c7fd
Samraggi Pyarial <samraggi.p67@rsu.ac.th>
2025-11-04 18:43:07 +0700

    Add my name

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,7 +44,7 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
-        <h1 className="title">Attractions</h1>
+        <h1 className="title">Samraggi Pyarial 6708698</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
 
